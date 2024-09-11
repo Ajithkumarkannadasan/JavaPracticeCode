5eeccea1a2ce49dfaffd5f524816524ddcf0f83e
Ajithkumar.K <Ajithkumar.K@TWTPEN1912045AC.hq.intra.acer.com>
2024-09-11 06:49:47 +0530

    1st commit

---
+++ BankDetailsTask/src/org/bank/BankInfo.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class BankInfo {
+
+	public void saving() {
+		System.out.println("Savings 6 %");
+	}
+	public void fixed() {
+		System.out.println("Fixed : 7%");
+		
+	}
+	
+	public void deposit() {
+		System.out.println("Deposit : 2%");
+	}
+}
