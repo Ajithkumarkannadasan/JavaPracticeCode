c2f4c08faa5747691d5e9fc69824aecd603689fa
Ajithkumar.K <Ajithkumar.K@TWTPEN1912045AC.hq.intra.acer.com>
2025-01-30 13:54:05 +0530

    Methods2

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,27 @@
+package org.add;
+
+public class GreensTech {
+	
+	public void greensOmr() {
+		System.out.println("OMR branch name is greensOmr");
+	}
+
+	public void greensAdayar() {
+		System.out.println("OMR branch name is greensAdayar");
+	}
+	public void greensTambaram() {
+		System.out.println("OMR branch name is greensTambaram");
+	}
+	public void greensVelacherry() {
+		System.out.println("OMR branch name is greensOmr");
+	}
+	
+	public static void main (String[]args) {
+		GreensTech g = new GreensTech();
+		
+		g.greensAdayar();
+		g.greensOmr();
+		g.greensTambaram();
+		g.greensVelacherry();
+	}
+}
